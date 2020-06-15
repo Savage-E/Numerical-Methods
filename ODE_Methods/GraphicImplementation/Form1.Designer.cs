0fab4eaa3f8822c0ae347c628c38e4efd661448e
Vladislav Kotov <vlad.kotov1999@ya.ru>
2020-06-16 00:17:42 +0300

    Add files via upload
    The implementation of Numerical methods of solving ordinary differential equations of 1st order:
    Euler method,
    Modified Euler method,
    Runge-Kutta method,
    Adams method.

---
+++ ODE_Methods/GraphicImplementation/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace GraphicImplementation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dv = new System.Windows.Forms.DataGridView();
+            this.Xi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Yi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modified_Euler = new System.Windows.Forms.RadioButton();
+            this.Euler = new System.Windows.Forms.RadioButton();
+            this.Rugle_Kutte = new System.Windows.Forms.RadioButton();
+            this.Adams = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dv
+            // 
+            this.dv.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dv.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Xi,
+            this.Yi});
+            this.dv.GridColor = System.Drawing.Color.White;
+            this.dv.Location = new System.Drawing.Point(796, 12);
+            this.dv.Name = "dv";
+            this.dv.RowHeadersVisible = false;
+            this.dv.Size = new System.Drawing.Size(207, 549);
+            this.dv.TabIndex = 0;
+            // 
+            // Xi
+            // 
+            this.Xi.HeaderText = "Xi";
+            this.Xi.Name = "Xi";
+            this.Xi.ReadOnly = true;
+            // 
+            // Yi
+            // 
+            this.Yi.HeaderText = "Yi";
+            this.Yi.Name = "Yi";
+            this.Yi.ReadOnly = true;
+            // 
+            // Modified_Euler
+            // 
+            this.Modified_Euler.AutoSize = true;
+            this.Modified_Euler.Location = new System.Drawing.Point(894, 377);
+            this.Modified_Euler.Name = "Modified_Euler";
+            this.Modified_Euler.Size = new System.Drawing.Size(92, 17);
+            this.Modified_Euler.TabIndex = 1;
+            this.Modified_Euler.TabStop = true;
+            this.Modified_Euler.Text = "Modified Euler";
+            this.Modified_Euler.UseVisualStyleBackColor = true;
+            this.Modified_Euler.CheckedChanged += new System.EventHandler(this.Modified_Euler_CheckedChanged);
+            // 
+            // Euler
+            // 
+            this.Euler.AutoSize = true;
+            this.Euler.Location = new System.Drawing.Point(894, 400);
+            this.Euler.Name = "Euler";
+            this.Euler.Size = new System.Drawing.Size(49, 17);
+            this.Euler.TabIndex = 2;
+            this.Euler.TabStop = true;
+            this.Euler.Text = "Euler";
+            this.Euler.UseVisualStyleBackColor = true;
+            this.Euler.CheckedChanged += new System.EventHandler(this.Euler_CheckedChanged);
+            // 
+            // Rugle_Kutte
+            // 
+            this.Rugle_Kutte.AutoSize = true;
+            this.Rugle_Kutte.Location = new System.Drawing.Point(894, 423);
+            this.Rugle_Kutte.Name = "Rugle_Kutte";
+            this.Rugle_Kutte.Size = new System.Drawing.Size(81, 17);
+            this.Rugle_Kutte.TabIndex = 3;
+            this.Rugle_Kutte.TabStop = true;
+            this.Rugle_Kutte.Text = "Rugle-Kutte";
+            this.Rugle_Kutte.UseVisualStyleBackColor = true;
+            this.Rugle_Kutte.CheckedChanged += new System.EventHandler(this.Rugle_Kutte_CheckedChanged);
+            // 
+            // Adams
+            // 
+            this.Adams.AutoSize = true;
+            this.Adams.Location = new System.Drawing.Point(894, 446);
+            this.Adams.Name = "Adams";
+            this.Adams.Size = new System.Drawing.Size(57, 17);
+            this.Adams.TabIndex = 4;
+            this.Adams.TabStop = true;
+            this.Adams.Text = "Adams";
+            this.Adams.UseVisualStyleBackColor = true;
+            this.Adams.CheckedChanged += new System.EventHandler(this.Adams_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 584);
+            this.Controls.Add(this.Adams);
+            this.Controls.Add(this.Rugle_Kutte);
+            this.Controls.Add(this.Euler);
+            this.Controls.Add(this.Modified_Euler);
+            this.Controls.Add(this.dv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GraphicsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Xi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Yi;
+        private System.Windows.Forms.RadioButton Modified_Euler;
+        private System.Windows.Forms.RadioButton Euler;
+        private System.Windows.Forms.RadioButton Rugle_Kutte;
+        private System.Windows.Forms.RadioButton Adams;
+    }
+}
+
---
+++ ODE_Methods/GraphicImplementation/Form1.cs
@@ -0,0 +1,199 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GraphicImplementation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Right part of y'=f(x,y)
+        static public double func(double x, double y)
+        {
+            return x * x - 2 * y;
+        }
+
+        private double a, b; //The range on which is being calculating y(x).
+        private double h; // Step.
+        private int n; // Amount of points on which are being calculated the decision.
+
+        private double[] x;
+        private double[] y;
+        private double[] f;
+
+        public PlotView pv = new PlotView
+        {
+            Location = new Point(0, 0),
+            Size = new Size(750, 750),
+            Model = new PlotModel { Title = "f(x,y)=x^2-2y  y(0)=1 " },
+        };
+
+        private void Modified_Euler_CheckedChanged(object sender, EventArgs e)
+        {
+            dv.Rows.Clear();
+
+            pv.Model = new PlotModel { Title = "f(x,y)=x^2-3*y  y(0)=1 Midified Euler" };
+            FunctionSeries fs = new FunctionSeries();
+            pv.Model.Series.Add(fs);
+            fs.Color = OxyColors.Black;
+            fs.MarkerStroke = OxyColors.Red;
+            fs.MarkerType = MarkerType.Cross;
+            fs.MarkerSize = 5;
+
+            x = new double[n];
+            y = new double[n];
+
+            x[0] = 0;
+            y[0] = 1;
+
+            double y12;
+            for (int i = 0; i < n - 1; i++)
+            {
+                y12 = y[i] + h / 2 * func(x[i], y[i]);
+                y[i + 1] = y[i] + h * func(x[i] + h / 2, y12);
+                x[i + 1] = x[i] + h;
+                fs.Points.Add(new DataPoint(x[i + 1], y[i + 1]));
+            }
+            ShowPoints();
+        }
+
+        private void Euler_CheckedChanged(object sender, EventArgs e)
+        {
+            pv.Model = new PlotModel { Title = "f(x,y)=x^2-3*y  y(0)=1 Euler" };
+            dv.Rows.Clear();
+            FunctionSeries fs = new FunctionSeries();
+            pv.Model.Series.Add(fs);
+            fs.Color = OxyColors.Black;
+            fs.MarkerStroke = OxyColors.Red;
+            fs.MarkerType = MarkerType.Cross;
+            fs.MarkerSize = 5;
+
+            x = new double[n];
+            y = new double[n];
+
+            x[0] = 0;
+            y[0] = 1;
+
+            double yii;
+            for (int i = 0; i < n - 1; i++)
+            {
+                yii = y[i] + h * func(x[i], y[i]);
+                y[i + 1] = y[i] + h / 2 * (func(x[i], y[i]) + func(x[i] + h, yii));
+                x[i + 1] = x[i] + h;
+                fs.Points.Add(new DataPoint(x[i + 1], y[i + 1]));
+            }
+
+            ShowPoints();
+        }
+
+        private void GraphicsForm_Load(object sender, EventArgs e)
+        {
+            Controls.Add(pv);
+            a = 0;
+            b = 1;
+            h = 0.1;
+            n = (int)Math.Ceiling((b - a) / h) + 1;
+
+            x[0] = 0;
+            y[0] = 1;
+        }
+
+        private void Rugle_Kutte_CheckedChanged(object sender, EventArgs e)
+        {
+            pv.Model = new PlotModel { Title = "f(x,y)=x^2-3*y  y(0)=1 Runge-Kutta" };
+            dv.Rows.Clear();
+            FunctionSeries fs = new FunctionSeries();
+            pv.Model.Series.Add(fs);
+            fs.Color = OxyColors.Black;
+            fs.MarkerStroke = OxyColors.Red;
+            fs.MarkerType = MarkerType.Cross;
+            fs.MarkerSize = 5;
+
+            x = new double[n];
+            y = new double[n];
+
+            x[0] = 0;
+            y[0] = 1;
+
+            double k0, k1, k2, k3;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                k0 = func(x[i], y[i]);
+                k1 = func(x[i] + h / 2, y[i] + h * k0 / 2);
+                k2 = func(x[i] + h / 2, y[i] + h * k1 / 2);
+                k3 = func(x[i] + h, y[i] + h * k2);
+                y[i + 1] = y[i] + h / 6 * (k0 + 2 * k1 + 2 * k2 + k3);
+                x[i + 1] = x[i] + h;
+                fs.Points.Add(new DataPoint(x[i + 1], y[i + 1]));
+            }
+
+            ShowPoints();
+        }
+
+        private void Adams_CheckedChanged(object sender, EventArgs e)
+        {
+            pv.Model = new PlotModel { Title = "f(x,y)=x^2-3*y  y(0)=1 Adams" };
+            dv.Rows.Clear();
+            FunctionSeries fs = new FunctionSeries();
+            pv.Model.Series.Add(fs);
+            fs.Color = OxyColors.Black;
+            fs.MarkerStroke = OxyColors.Red;
+            fs.MarkerType = MarkerType.Cross;
+            fs.MarkerSize = 5;
+
+            x = new double[n];
+            y = new double[n];
+            f = new double[n];
+
+            x[0] = 0;
+            y[0] = 1;
+            f[0] = func(x[0], y[0]);
+
+            //Calculating the approximate value in the 1st point by Euler's method.
+            x[1] = x[0] + h;
+            y[1] = y[0] + h * f[0];
+            f[1] = func(x[1], y[1]);
+
+            //Calculating the approximate value in the 2nd point by 2nd order Adams method.
+            x[2] = x[1] + h;
+            y[2] = y[1] + h * (3 / 2 * f[1] - 1 / 2 * f[0]);
+            f[2] = func(x[2], y[2]);
+
+            //Calculating the approximate value in the 3rd point by 3rd order Adams method.
+            x[3] = x[2] + h;
+            y[3] = y[2] + h / 12 * (23 * f[2] - 16 * f[1] + 5 * f[0]);
+            f[3] = func(x[3], y[3]);
+
+            //Calculating approximate values in subsequent points by 4th order Adams method.
+            for (int i = 3; i < n - 1; i++)
+            {
+                y[i + 1] = y[i] + h / 24 * (55 * f[i] - 59 * f[i - 1] + 37 * f[i - 2] - 9 * f[i - 3]);
+                x[i + 1] = x[i] + h;
+                f[i + 1] = func(x[i + 1], y[i + 1]);
+                fs.Points.Add(new DataPoint(x[i + 1], y[i + 1]));
+            }
+            ShowPoints();
+        }
+
+        /// <summary>
+        /// Showing points in the grid.
+        /// </summary>
+        private void ShowPoints()
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                dv.Rows.Add();
+                dv.Rows[i].Cells["Xi"].Value = x[i + 1];
+                dv.Rows[i].Cells["Yi"].Value = y[i + 1];
+            }
+        }
+    }
+}
